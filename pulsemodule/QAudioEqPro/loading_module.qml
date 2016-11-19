bd0b0312ac30dfe382ae6cca5a2775de6d185ba2
Stefano Belli <sa.belli@hotmail.it>
2016-11-19 10:35:19 +0100

    Update QAudioEqPro

--- pulsemodule/QAudioEqPro/loading_module.qml
+++ pulsemodule/QAudioEqPro/loading_module.qml
@@ -4,6 +4,11 @@ import QtQuick.Dialogs 1.2
 Dialog {
     visible: true
     title: qsTr("Loading pulseaudio module...");
+    Connections {
+        target: sig
+
+    }
+
     Text {
         id: dialogLoadingAlert
         text: "Please wait while connecting to Pulse and loading the module..."
--- pulsemodule/QAudioEqPro/main.cpp
+++ pulsemodule/QAudioEqPro/main.cpp
@@ -1,14 +1,29 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlComponent>
+#include <QQmlContext>
+
 #include "q_pamod.h"
 
 #define MODULE_NAME "module-equalizer-sink"
+#define MODULE_PARAMS ""
+
+QObject* createQmlCompo(QQmlEngine& engine, const QString &localFile)
+{
+    QQmlComponent qml_compo(&engine,
+                            QUrl::fromLocalFile(localFile));
+    return qml_compo.create();
+}
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc,argv);
+    QQmlEngine qml_engine;
+    QObject* loadingDialog = createQmlCompo(qml_engine,"loading_module.qml");
+    QQmlContext* ctx = qml_engine.rootContext();
+    ctx->setContextProperty("sig",nullptr);
 
-    if(qpa::loadModule(MODULE_NAME,"")) {
+    if(qpa::loadModule(MODULE_NAME,MODULE_PARAMS)) {
         //use dlopen to load shared object
     }
 
